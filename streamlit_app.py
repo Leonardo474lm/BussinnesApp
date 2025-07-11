74a4683daa0cae02f72551b008dbf2e98bbb5fc1
Leonardo <arthur123piero123@gmail.com>
2025-07-11 18:47:29 -0500

    actualizaciono

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,17 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-%matplotlib inline
-
-#Automcompletar rápido
-%config IPCompleter.greedy=True
-#Desactivar la notación científica
-#Desactivar la notación científica
 pd.options.display.float_format = '{:.3f}'.format
-
-#Google Drive
-from google.colab import drive
-drive.mount('/content/drive')
 
 df = pd.read_csv("/content/drive/MyDrive/TF/Grupo_Bussiness/heart_attack_prediction_dataset.csv", encoding='ISO-8859-1', sep=",")
 df.head(4)
