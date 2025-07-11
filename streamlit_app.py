b3b7a908ac889359c592c3a16f2a8141f6102737
Leonardo <arthur123piero123@gmail.com>
2025-07-11 18:52:34 -0500

    agregadfon data

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,18 @@
 import pandas as pd
+import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
-
+import requests
+from io import StringIO
 pd.options.display.float_format = '{:.3f}'.format
+url = st.text_input("Pega el enlace público del archivo CSV en Google Drive")
 
-df = pd.read_csv("/content/drive/MyDrive/TF/Grupo_Bussiness/heart_attack_prediction_dataset.csv", encoding='ISO-8859-1', sep=",")
-df.head(4)
-df.info()
-print(df.shape)
+if url:
+    try:
+        file_id = url.split("/")[-2]
+        dwn_url = f'https://drive.google.com/uc?id={file_id}'
+        df = pd.read_csv(dwn_url)
+        st.write("Datos cargados:")
+        st.dataframe(df.head())
+    except Exception as e:
+        st.error(f"Error al cargar el archivo: {e}")
